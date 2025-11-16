9590525177509b83f9b8c1f38ab7416f6673c5a6
Loseq <loseq@proton.me>
2025-11-16 13:49:28 +0100

    add sync

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ type Database = Item | Column
 
 const database = new PouchDB<Database>("todo-list-cdl")
 
+database.sync(import.meta.env.COUCH_URL)
 
 const columns = usePouchRef<Column, Database>({
   selector: {
